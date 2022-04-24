5b1353983345c6eeede28844ac51a44a5c222869
chin <a5566qq2581@gmail.com>
2022-04-24 23:06:40 +0800

    feat: add swagger-jsdoc to generate openapi

---
+++ get-swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+
+(async () => {
+    const swaggerDefinition = {
+        info: {
+            // API informations (required)
+            title: 'Raccoon', // Title (required)
+            version: '1.0.0', // Version (required)
+            description: 'Raccoon API', // Description (optional)
+        },
+        openapi: '3.0.0',
+    };
+    
+    // Options for the swagger docs
+    const options = {
+        // Import swaggerDefinitions
+        swaggerDefinition,
+        // Path to the API docs
+        // Note that this path is relative to the current directory from which the Node.js is ran, not the application itself.
+        apis: [`${__dirname}/api/**/*.js`]
+    };
+    
+    const swaggerSpec = await swaggerJsDoc(options);
+    console.log(JSON.stringify(swaggerSpec, null, 4));
+})();
