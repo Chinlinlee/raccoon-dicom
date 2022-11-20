78de8aae0ac5159373181a779d22ea59f2c68370
chin <a5566qq2581@gmail.com>
2022-11-20 15:58:01 +0800

    refactor: move all module.exports to bottom

--- utils/log.js
+++ utils/log.js
@@ -4,10 +4,10 @@ const { configure, getLogger } = require("log4js");
 configure(path.join(__dirname, "../config/log4js.default.json"));
 let raccoonLogger = getLogger("raccoon-polka");
 
-module.exports.logger = raccoonLogger;
-
 let raccoonFHIRLogger = getLogger("raccoon-polka-fhir");
-module.exports.fhirLogger = raccoonFHIRLogger;
 
 let raccoonPythonLogger = getLogger("raccoon-polka-python");
+
+module.exports.logger = raccoonLogger;
+module.exports.fhirLogger = raccoonFHIRLogger;
 module.exports.pythonLogger = raccoonPythonLogger;
