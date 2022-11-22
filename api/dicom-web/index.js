e767caf2dfebdb02e2df7aa2b8b42cdfaa73f40e
chin <a5566qq2581@gmail.com>
2022-11-22 16:52:42 +0800

    docs: apidoc of retrieve metadata of each level

--- api/dicom-web/index.js
+++ api/dicom-web/index.js
@@ -246,14 +246,61 @@ app.get(
 
 //#region WADO-RS Retrieve Transaction Metadata Resources
 
+/**
+ *  @openapi
+ *  /dicom-web/studies/{studyUID}/metadata:
+ *    get:
+ *      tags:
+ *        - WADO-RS
+ *      description: Retrieve Study's instances' metadata
+ *      parameters:
+ *        - $ref: "#/components/parameters/studyUID"
+ *      responses:
+ *        200:
+ *          $ref: "#/components/responses/DicomMetadata"
+ *          
+ */
 app.get(
     "/studies/:studyUID/metadata",
     require("./controller/WADO-RS/retrieveStudyMetadata")
 );
+
+/**
+ *  @openapi
+ *  /dicom-web/studies/{studyUID}/series/{seriesUID}/metadata:
+ *    get:
+ *      tags:
+ *        - WADO-RS
+ *      description: Retrieve Study's series' instances' metadata
+ *      parameters:
+ *        - $ref: "#/components/parameters/studyUID"
+ *        - $ref: "#/components/parameters/seriesUID"
+ *      responses:
+ *        200:
+ *          $ref: "#/components/responses/DicomMetadata"
+ *          
+ */
 app.get(
     "/studies/:studyUID/series/:seriesUID/metadata",
     require("./controller/WADO-RS/retrieveSeriesMetadata")
 );
+
+/**
+ *  @openapi
+ *  /dicom-web/studies/{studyUID}/series/{seriesUID}/instances/{instanceUID}/metadata:
+ *    get:
+ *      tags:
+ *        - WADO-RS
+ *      description: Retrieve instance's metadata
+ *      parameters:
+ *        - $ref: "#/components/parameters/studyUID"
+ *        - $ref: "#/components/parameters/seriesUID"
+ *        - $ref: "#/components/parameters/instanceUID"
+ *      responses:
+ *        200:
+ *          $ref: "#/components/responses/DicomMetadata"
+ *          
+ */
 app.get(
     "/studies/:studyUID/series/:seriesUID/instances/:instanceUID/metadata",
     require("./controller/WADO-RS/retrieveInstanceMetadata")
