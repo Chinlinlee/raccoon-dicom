e5791f60dbe3540db3b0c0ea3ec6a5a4766021b5
chin <a5566qq2581@gmail.com>
2022-12-26 19:04:32 +0800

    docs: move STOW-RS openapi into route

--- api/dicom-web/index.js
+++ api/dicom-web/index.js
@@ -227,6 +227,29 @@ app.get(
 
 //#region STOW-RS
 
+/**
+ *  @openapi
+ *  /dicom-web/studies:
+ *    post:
+ *      tags:
+ *        - STOW-RS
+ *      description: store DICOM instance
+ *      requestBody:
+ *        content:
+ *          multipart/related:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                file:
+ *                  type: string
+ *                  format: binary
+ *            encoding:
+ *              file:
+ *                contentType: application/dicom;
+ *      responses:
+ *        "200":
+ *          description: The DICOM instance store successfully
+ */
 app.post("/studies", require("./controller/STOW-RS/storeInstance"));
 
 //#endregion
