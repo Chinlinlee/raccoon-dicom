ad1f4d1c62ffbd20cbc575a01898c9cb5709b297
chin <a5566qq2581@gmail.com>
2023-03-29 15:46:47 +0800

    feat(dcm2json): correct DICOM when missing charset

--- models/DICOM/dicom-json-parser.js
+++ models/DICOM/dicom-json-parser.js
@@ -1,15 +1,38 @@
+const { DicomUtf8Converter } = require("./dcm4che/DicomUtf8Converter");
 const {
     dcm2jsonV8
 } = require("./dcmtk");
+const { logger } = require("../../utils/log");
+
 
 class DicomJsonParser {
     constructor() {}
 
     async parseFromFilename(filename) {
+        let dicomJson;
         try {
-            let dicomJson = await dcm2jsonV8.exec(filename);
+            dicomJson = await dcm2jsonV8.exec(filename);
             return dicomJson;
         } catch (e) {
+
+            /**
+             * EXITCODE_CANNOT_CONVERT_TO_UNICODE is usually due to dicom file missing (0008,0005)
+             * To fix this error, we use dcmconv to convert DICOM file to UTF-8 (ISO_IR 192)
+             */
+            if (e.message.includes("EXITCODE_CANNOT_CONVERT_TO_UNICODE")) {
+                logger.warn(`The file: ${filename} may missing/incorrect (0008,0005) charset, converter dicom to UTF8`);
+
+                try {
+                    let dicomUtf8Converter = new DicomUtf8Converter(filename);
+                    await dicomUtf8Converter.convert();
+                
+                    dicomJson = await dcm2jsonV8.exec(filename);
+                    return dicomJson;
+                } catch(e) {
+                    throw e;
+                }
+            }
+
             throw e;
         }
     }
