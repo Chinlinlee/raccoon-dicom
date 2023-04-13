2875d88dfc9f33311dc22ff631215c3b5233ac71
chin <a5566qq2581@gmail.com>
2023-04-13 21:05:52 +0800

    test: stop mongodb after test

--- test/before.js
+++ test/before.js
@@ -5,4 +5,8 @@ before(async() => {
     const mongoServer = await MongoMemoryServer.create();
     await mongoose.connect(mongoServer.getUri(), { dbName: "raccoon-dicom" });
     console.log("connected mongoose");
+    after(async()=> {
+        await mongoServer.stop();
+        await mongoose.disconnect();
+    });
 });
