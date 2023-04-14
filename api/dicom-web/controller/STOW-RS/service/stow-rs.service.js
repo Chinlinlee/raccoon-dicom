706561047c2acb1cf5d7119c8e55f13089c63d1d
chin <a5566qq2581@gmail.com>
2023-04-14 20:09:40 +0800

    refactor: extract `storeInstance` process into fn

--- api/dicom-web/controller/STOW-RS/service/stow-rs.service.js
+++ api/dicom-web/controller/STOW-RS/service/stow-rs.service.js
@@ -1,12 +1,12 @@
 const uuid = require("uuid");
 const _ = require("lodash");
 const { DicomJsonParser } = require("../../../../../models/DICOM/dicom-json-parser");
-const { 
+const {
     DicomJsonModel,
     DicomJsonBinaryDataModel
 } = require("../../../../../models/DICOM/dicom-json-model");
 const { DicomFileSaver } = require("./dicom-file-saver");
-const { DicomFhirService} = require("./dicom-fhir.service");
+const { DicomFhirService } = require("./dicom-fhir.service");
 const { DicomJpegGenerator } = require("./dicom-jpeg-generator");
 const { logger } = require("../../../../../utils/logs/log");
 
@@ -82,37 +82,10 @@ class StowRsService {
 
             let currentFile = this.uploadFiles[i];
 
-            let dicomJsonParser = new DicomJsonParser();
-            let dicomJson = await dicomJsonParser.parseFromFilename(currentFile.filepath);
-
-            let dicomJsonModel = new DicomJsonModel(dicomJson);
-            dicomJsonModel.setMinifyDicomJsonAndTempBigValueTags();
-            dicomJsonModel.setUidObj();
-
-            let isSameStudyIDStatus = this.isSameStudyID_(this.responseMessage);
-            if (!isSameStudyIDStatus) {
-                this.responseCode = 409;
-            }
-
-            let dicomJsonBinaryDataModel = new DicomJsonBinaryDataModel(dicomJsonModel);
-            await dicomJsonBinaryDataModel.storeAllBinaryDataToFileAndDb();
-            dicomJsonBinaryDataModel.replaceAllBinaryToURI();
-
-            let dicomFileSaver = new DicomFileSaver(currentFile, dicomJsonModel);
-            let dicomFileSaveInfo = await dicomFileSaver.saveAndGetSaveInfo();
-
-            await dicomJsonModel.saveMetadataToFile(dicomFileSaveInfo.fullPath);
-
-            await dicomJsonModel.storeToDb(dicomFileSaveInfo);
-
-            let retrieveUrlObj = this.getRetrieveUrl(dicomJsonModel.uidObj);
-            this.responseMessage["00081190"].Value.push(retrieveUrlObj.study);
-            this.responseMessage["00081190"].Value = _.uniq(this.responseMessage["00081190"].Value);
-
-            let sopSeq = this.getSOPSeq(dicomJsonModel.uidObj.sopClass, dicomJsonModel.uidObj.sopInstanceUID);
-            _.set(sopSeq, "00081190.vr", "UT");
-            _.set(sopSeq, "00081190.Value", [retrieveUrlObj.instance]);
-            this.responseMessage["00081199"]["Value"].push(sopSeq);
+            let {
+                dicomJsonModel,
+                dicomFileSaveInfo
+            } = await this.storeInstance(currentFile);
 
 
             //sync DICOM to FHIR
@@ -130,6 +103,49 @@ class StowRsService {
         return {
             code: this.responseCode,
             responseMessage: this.responseMessage
+        };
+    }
+
+    /**
+     * 
+     * @param {import("formidable").File} file 
+     */
+    async storeInstance(file) {
+        let dicomJsonParser = new DicomJsonParser();
+        let dicomJson = await dicomJsonParser.parseFromFilename(file.filepath);
+
+        let dicomJsonModel = new DicomJsonModel(dicomJson);
+        dicomJsonModel.setMinifyDicomJsonAndTempBigValueTags();
+        dicomJsonModel.setUidObj();
+
+        let isSameStudyIDStatus = this.isSameStudyID_(this.responseMessage);
+        if (!isSameStudyIDStatus) {
+            this.responseCode = 409;
+        }
+
+        let dicomJsonBinaryDataModel = new DicomJsonBinaryDataModel(dicomJsonModel);
+        await dicomJsonBinaryDataModel.storeAllBinaryDataToFileAndDb();
+        dicomJsonBinaryDataModel.replaceAllBinaryToURI();
+
+        let dicomFileSaver = new DicomFileSaver(file, dicomJsonModel);
+        let dicomFileSaveInfo = await dicomFileSaver.saveAndGetSaveInfo();
+
+        await dicomJsonModel.saveMetadataToFile(dicomFileSaveInfo.fullPath);
+
+        await dicomJsonModel.storeToDb(dicomFileSaveInfo);
+
+        let retrieveUrlObj = this.getRetrieveUrl(dicomJsonModel.uidObj);
+        this.responseMessage["00081190"].Value.push(retrieveUrlObj.study);
+        this.responseMessage["00081190"].Value = _.uniq(this.responseMessage["00081190"].Value);
+
+        let sopSeq = this.getSOPSeq(dicomJsonModel.uidObj.sopClass, dicomJsonModel.uidObj.sopInstanceUID);
+        _.set(sopSeq, "00081190.vr", "UT");
+        _.set(sopSeq, "00081190.Value", [retrieveUrlObj.instance]);
+        this.responseMessage["00081199"]["Value"].push(sopSeq);
+
+        return {
+            dicomJsonModel,
+            dicomFileSaveInfo
         };
     }
 
@@ -205,7 +221,7 @@ class StowRsService {
     getRetrieveUrl(uidObj) {
         let dicomWebService = new DicomWebService(this.request, undefined);
         let url = `${dicomWebService.getBasicURL()}/studies`;
-    
+
         return {
             study: `${url}/${uidObj.studyUID}`,
             series: `${url}/${uidObj.studyUID}/series/${uidObj.seriesUID}`,
