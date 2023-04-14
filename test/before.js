2e215385ba783d65309614388ea7e2f97376241e
chin <a5566qq2581@gmail.com>
2023-04-15 00:47:58 +0800

    test: disable logger when test

--- test/before.js
+++ test/before.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
+const {getLogger} = require("log4js");
 
 before(async() => {
+    getLogger().level = "off";
+    getLogger("raccoon-polka").level = "off";
+    getLogger("raccoon-polka-fhir").level = "off";
+    getLogger("api").level = "off";
+
     const mongoServer = await MongoMemoryServer.create();
     await mongoose.connect(mongoServer.getUri(), { dbName: "raccoon-dicom" });
     console.log("connected mongoose");
