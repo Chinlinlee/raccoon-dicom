a033301dc0c055a09145736db3aaa7c2e19cfe60
chin <a5566qq2581@gmail.com>
2023-04-15 00:48:55 +0800

    test: add mocha config

---
+++ .mocharc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "diff": true,
+    "extension": ["js", "cjs", "mjs"],
+    "package": "./package.json",
+    "reporter": "spec",
+    "slow": "75",
+    "timeout": "300000",
+    "ui": "bdd",
+    "parallel": false,
+    "sort": false,
+    "recursive": true
+};
