b320f180c77436de6f0896c173c8ae3fc5a42461
chin <a5566qq2581@gmail.com>
2023-04-29 13:18:43 +0800

    Revert "feat(wado-rs): send `content-length` of `zip`"
    This reverts commit 281927fbf9ef1d6b55df884fc351cf863d1a8faf.

--- api/dicom-web/controller/WADO-RS/service/WADOZip.js
+++ api/dicom-web/controller/WADO-RS/service/WADOZip.js
@@ -3,9 +3,6 @@ const archiver = require("archiver");
 const wadoService = require("./WADO-RS.service");
 const path = require("path");
 const dicomModel = require("../../../../../models/mongodb/models/dicom");
-const fs = require("fs");
-const uuid = require("uuid");
-const { Writable } = require("stream");
 
 class WADOZip {
     constructor(iParam, iRes) {
@@ -75,39 +72,12 @@ class WADOZip {
     }
 }
 
-/**
- * 
- * @param {import('express').Response} res 
- * @param {string[]} folders 
- * @param {string} filename 
- * @returns 
- */
 function toZip(res, folders = [], filename = "") {
     return new Promise((resolve) => {
-
-        const bufferArray = [];
-        const converter = new Writable();
-        converter._write = (chunk, encoding, cb) => {
-            bufferArray.push(chunk);
-            process.nextTick(cb);
-        };
-
-        converter.on('finish', () => {
-            let fullBuffer = Buffer.concat(bufferArray);
-            res.setHeader("Content-Length", fullBuffer.length);
-            res.write(fullBuffer);
-            resolve({
-                status: true,
-                code: 200,
-                data: "success"
-            });
-        });
-
         let archive = archiver('zip', {
             gzip: true,
             zlib: { level: 9 } // Sets the compression level.
         });
-
         archive.on('error', function (err) {
             console.error(err);
             resolve({
@@ -116,9 +86,7 @@ function toZip(res, folders = [], filename = "") {
                 data: err
             });
         });
-
-        archive.pipe(converter);
-
+        archive.pipe(res);
         if (folders.length > 0) {
             for (let i = 0; i < folders.length; i++) {
                 let folderName = path.basename(folders[i]);
@@ -126,12 +94,15 @@ function toZip(res, folders = [], filename = "") {
                 archive.glob("*.dcm", { cwd: folders[i] }, { prefix: folderName });
             }
         } else {
-            archive.file(filename, {
-                name: path.basename(filename)
-            });
+            archive.file(filename);
         }
-
-        archive.finalize();
+        archive.finalize().then(() => {
+            resolve({
+                status: true,
+                code: 200,
+                data: "success"
+            });
+        });
     });
 }
 
