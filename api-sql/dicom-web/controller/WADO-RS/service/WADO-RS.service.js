1ff496d2f94679ac8acac49c8f0ebb59f0ce55c6
CHIN\a5566 <a5566qq2581@gmail.com>
2023-08-14 16:41:30 +0800

    refactor: remove duplicate code

--- api-sql/dicom-web/controller/WADO-RS/service/WADO-RS.service.js
+++ api-sql/dicom-web/controller/WADO-RS/service/WADO-RS.service.js
@@ -1,69 +1,16 @@
 const _ = require("lodash");
-const path = require("path");
 const fsP = require("fs/promises");
-const { MultipartWriter } = require("@root/utils/multipartWriter");
-const errorResponse = require("@root/utils/errorResponse/errorResponseMessage");
-const { raccoonConfig } = require("@root/config-class");
-const { JSONPath } = require("jsonpath-plus");
-const { DicomWebService } = require("@root/api/dicom-web/service/dicom-web.service");
-const { logger } = require("@root/utils/logs/log");
 const { InstanceModel } = require("@models/sql/models/instance.model");
 const { StudyModel } = require("@models/sql/models/study.model");
 const { SeriesModel } = require("@models/sql/models/series.model");
-
-/**
- *
- * @param {import("http").IncomingMessage} req
- * @return { string }
- */
-function getAcceptType(req) {
-    return req.headers.accept
-        .match(/type=(.*)/gi)[0]
-        .split(/[,;]/)[0]
-        .substring(5)
-        .replace(/"/g, "");
-}
-
-class ImageMultipartWriter {
-    /**
-     * 
-     * @param {import('express').Request} req 
-     * @param {import('express').Response} res 
-     * @param {typeof ImagePathFactory} imagePathFactory 
-     * @param {typeof ContentTypeWriter} contentTypeWriter 
-     */
-    constructor(req, res, imagePathFactory, contentTypeWriter) {
-        this.request = req;
-        this.response = res;
-        this.imagePathFactory = new imagePathFactory(req.params);
-        this.contentTypeWriterClass = contentTypeWriter;
-    }
-
-    async write() {
-        await this.imagePathFactory.getImagePaths();
-        let checkAllImageExistResult = await this.imagePathFactory.checkAllImageExist();
-        this.response.statusCode = checkAllImageExistResult.code;
-        if (!checkAllImageExistResult.status) {
-            this.response.setHeader("Content-Type", "application/dicom+json");
-            return this.response.json(checkAllImageExistResult);
-        }
-        logger.info(`retrieve study's images: ${this.imagePathFactory.getPartialImagesPathString()}`);
-
-        /** @type { ContentTypeWriter } */
-        let contentTypeWriter = new this.contentTypeWriterClass(
-            this.imagePathFactory.imagePaths,
-            this.request,
-            this.response
-        );
-        let writeResult = await contentTypeWriter.write();
-        if (!writeResult.status) {
-            this.response.setHeader("Content-Type", "application/dicom+json");
-            return this.response.status(writeResult.code).json(writeResult);
-        }
-
-        return this.response.end();
-    }
-}
+const { 
+    getAcceptType,
+    supportInstanceMultipartType, 
+    sendNotSupportedMediaType, 
+    addHostnameOfBulkDataUrl, 
+    multipartContentTypeWriter, 
+    ImageMultipartWriter 
+} = require("@root/api/dicom-web/controller/WADO-RS/service/WADO-RS.service");
 
 
 class ImagePathFactory {
@@ -170,76 +117,10 @@ class InstanceImagePathFactory extends ImagePathFactory {
     async getImagePaths() {
         let imagePath = await InstanceModel.getPathOfInstance(this.uids);
 
-        if(imagePath)
+        if (imagePath)
             this.imagePaths = [imagePath];
         else
             this.imagePaths = [];
-    }
-}
-
-class ContentTypeWriter {
-    constructor(imagePaths, req, res) {
-        this.imagePaths = imagePaths;
-        this.request = req;
-        this.response = res;
-    }
-
-    /**
-     * For image pre-processing, such as converting to compressed bulk data (i.e. image/jpeg, image/dicom-rle, etc.)
-     */
-    async preprocess() { }
-
-    async write() { }
-}
-
-class DicomTypeWriter extends ContentTypeWriter {
-    constructor(imagePaths, req, res) {
-        super(imagePaths, req, res);
-    }
-
-    async preprocess() { }
-
-    async write() {
-        let multipartWriter = new MultipartWriter(this.imagePaths, this.request, this.response);
-        return await multipartWriter.writeDICOMFiles("application/dicom");
-    }
-}
-
-const multipartContentTypeWriter = {
-    "application/dicom": DicomTypeWriter,
-    "application/octet-stream": DicomTypeWriter
-};
-
-const supportInstanceMultipartType = ["application/dicom", "application/octet-stream"];
-
-/**
- * 
- * @param { import('express').Response } res 
- * @param { string } type 
- * @returns 
- */
-function sendNotSupportedMediaType(res, type) {
-    let errorMessage = errorResponse.getNotSupportedErrorMessage(`The type ${type} is not supported, server supported \`multipart/related; type="application/dicom"\`, \`multipart/related; type="application/octet-stream"\` and \`application/zip\``);
-    res.writeHead(errorMessage.HttpStatus, {
-        "Content-Type": "application/dicom+json"
-    });
-    return res.end(JSON.stringify(errorMessage));
-}
-
-function addHostnameOfBulkDataUrl(metadata, req) {
-    let dicomWebService = new DicomWebService(req, undefined);
-
-    let urItems = JSONPath({
-        path: "$..BulkDataURI",
-        json: metadata,
-        resultType: "all"
-    });
-
-    for (let urItem of urItems) {
-        let bulkDataUriPath = JSONPath.toPathArray(urItem.path).join(".").substring(2);
-        let relativeUrl = _.get(metadata, bulkDataUriPath);
-
-        _.set(metadata, bulkDataUriPath, `${dicomWebService.getBasicURL()}${relativeUrl}`);
     }
 }
 
