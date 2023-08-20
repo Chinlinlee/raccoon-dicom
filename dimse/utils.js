faa7f6effd1994b4b9d802390cc24e42bd1b799d
chin <a5566qq2581@gmail.com>
2023-08-20 23:39:40 +0800

    fix: tag of dimse query is not correct
    # Problems
    - The request tag is lower case
    
    # Solutions
    - add toUpperCase for converting tag

--- dimse/utils.js
+++ dimse/utils.js
@@ -3,7 +3,7 @@
  * @param {number} tag 
  */
 function intTagToString(tag) {
-    return tag.toString(16).padStart(8, "0");
+    return tag.toString(16).padStart(8, "0").toUpperCase();
 }
 
 module.exports.intTagToString = intTagToString;
