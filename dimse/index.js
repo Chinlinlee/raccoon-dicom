54d0c4bdbd17ebf8ebf9151eb56d5401b917ce4f
CHIN\a5566 <a5566qq2581@gmail.com>
2023-08-21 13:31:12 +0800

    chore: remove unused require

--- dimse/index.js
+++ dimse/index.js
@@ -6,7 +6,6 @@ const path = require("path");
 
 const { ApplicationEntity } = require("@dcm4che/net/ApplicationEntity");
 const { BasicCEchoSCP } = require("@dcm4che/net/service/BasicCEchoSCP");
-const { Commands } = require("@dcm4che/net/Commands");
 const { Connection } = require("@dcm4che/net/Connection");
 const { Device } = require("@dcm4che/net/Device");
 const { DicomServiceRegistry } = require("@dcm4che/net/service/DicomServiceRegistry");
@@ -14,16 +13,6 @@ const { EnumSet } = require("@java-wrapper/java/util/EnumSet");
 const { QueryOption } = require("@dcm4che/net/QueryOption");
 const { TransferCapability } = require("@dcm4che/net/TransferCapability");
 const { TransferCapability$Role: TransferCapabilityRole } = require("@dcm4che/net/TransferCapability$Role");
-const { createCStoreSCPInjectProxy, default: CStoreSCPInject } = require("@chinlinlee/dcm777/net/CStoreSCPInject");
-const { default: Dimse } = require("@dcm4che/net/Dimse");
-const { Status } = require("@dcm4che/net/Status");
-const { default: Association } = require("@dcm4che/net/Association");
-const { default: PresentationContext } = require("@dcm4che/net/pdu/PresentationContext");
-const { default: Attributes } = require("@dcm4che/data/Attributes");
-const { default: DicomOutputStream } = require("@dcm4che/io/DicomOutputStream");
-const { default: File } = require("@java-wrapper/java/io/File");
-const { default: Tag } = require("@dcm4che/data/Tag");
-const { default: SimpleCStoreSCP } = require("@java-wrapper/org/github/chinlinlee/dcm777/net/SimpleCStoreSCP");
 const { JsCStoreScp } = require("./c-store");
 const { JsCFindScp } = require("./c-find");
 
