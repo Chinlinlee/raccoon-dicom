1088727a503c2cbb03d86bf00a00d479dd1be507
chin <a5566qq2581@gmail.com>
2023-08-26 22:48:08 +0800

    feat: #15 add onDicomInstancesAccessed

--- models/DICOM/audit/auditManager.js
+++ models/DICOM/audit/auditManager.js
@@ -281,6 +281,141 @@ class AuditManager {
         return await AuditManager.toJson(msg);
     }
 
+        /**
+     * A.X.3.6 DICOM Instances Accessed
+     * 
+     * Instances being viewed, utilized, updated, or deleted.
+     * 
+     * 1. 獲取 DICOM Instances Accessed (當有DICOM被存取時) 的訊息
+     * 2. 儲存 message 至 db
+     * 
+     * @param CRUD 該事件是屬於新增、讀取、更新、刪除哪一個? 必須為"C"、"R"、"U"、"D"其中一個，或是使用EventActionCode類別的CRUD(最終也會取得String)。
+     * @param eventResult 該事件最終的結果? 必須為"0"、"4"、"8"、"12"其中一個，分別對應到Success、MinorFailure、SeriousFailure、MajorFailure，或是使用EventOutcomeIndicator類別(最終也會取得String)。
+     * @param clientAETitle 發送者的AETitle
+     * @param clientHostname 發送者的位址
+     * @param serverAETitle 伺服器端的AETitle
+     * @param serverHostname 伺服器端的位址
+     * @param StudyInstanceUIDs 所有此次傳輸有關聯的StudyInstanceUID
+     * @param SOPClassUIDs 所有此次傳輸有關聯的SOPClassUID
+     * @param PatientID 一個此次傳輸關聯的PatientID
+     * @param PatientName 一個此次傳輸關聯的PatientName
+     */
+    async onDicomInstancesAccessed(CRUD, eventResult,
+        clientAETitle, clientHostname,
+        serverAETitle, serverHostname,
+        StudyInstanceUIDs, SOPClassUIDs,
+        PatientID, PatientName
+    ) {
+        let msg = await this.getDicomInstancesAccessedMsg(
+            CRUD, eventResult,
+            clientAETitle, clientHostname,
+            serverAETitle, serverHostname,
+            StudyInstanceUIDs, SOPClassUIDs,
+            PatientID, PatientName
+        );
+
+        await this.saveToDb_(msg);
+    }
+
+    /**
+     * A.X.3.6 DICOM Instances Accessed
+     * 
+     * 獲取 DICOM Instances Accessed (當有DICOM被存取時) 的訊息
+     * Instances being viewed, utilized, updated, or deleted.
+     * 
+     * @param CRUD 該事件是屬於新增、讀取、更新、刪除哪一個? 必須為"C"、"R"、"U"、"D"其中一個，或是使用EventActionCode類別的CRUD(最終也會取得String)。
+     * @param eventResult 該事件最終的結果? 必須為"0"、"4"、"8"、"12"其中一個，分別對應到Success、MinorFailure、SeriousFailure、MajorFailure，或是使用EventOutcomeIndicator類別(最終也會取得String)。
+     * @param clientAETitle 發送者的AETitle
+     * @param clientHostname 發送者的位址
+     * @param serverAETitle 伺服器端的AETitle
+     * @param serverHostname 伺服器端的位址
+     * @param StudyInstanceUIDs 所有此次傳輸有關聯的StudyInstanceUID
+     * @param SOPClassUIDs 所有此次傳輸有關聯的SOPClassUID
+     * @param PatientID 一個此次傳輸關聯的PatientID
+     * @param PatientName 一個此次傳輸關聯的PatientName
+     * @return {Promise<JSON>}
+     */
+    async getDicomInstancesAccessedMsg(CRUD, eventResult,
+        clientAETitle, clientHostname,
+        serverAETitle, serverHostname,
+        StudyInstanceUIDs, SOPClassUIDs,
+        PatientID, PatientName
+    ) {
+        /**
+        Event 
+        */
+        let theEvent = await EventIdentification.newInstanceAsync();
+        await theEvent.setEventID(EventID.DICOMInstancesAccessed);
+        await theEvent.setEventActionCode(CRUD);
+        await theEvent.setEventDateTime(await Calendar.getInstance()); // 日期時間為當前時間自動取得
+        await theEvent.setEventOutcomeIndicator(eventResult);
+
+        /**
+        Active Participant: 
+        Person and or Process manipulating the data (1..2)
+        存取資料的人或程式，這裡假定一定會有請求者(client)與我們本身(server)會去存取資料。
+        */
+        let server = await ActiveParticipant.newInstanceAsync();
+        await server.setUserID(serverAETitle);
+        await server.setAlternativeUserID(serverAETitle);
+        await server.setUserName("");
+        await server.setUserIsRequestor(false);
+        await (await server.getRoleIDCode()).add(AuditMessages$RoleIDCode.Destination);
+        await server.setNetworkAccessPointTypeCode(AuditMessages$NetworkAccessPointTypeCode.IPAddress);
+        await server.setNetworkAccessPointID(serverHostname);
+
+
+        let client = await ActiveParticipant.newInstanceAsync();
+        await client.setUserID(clientAETitle);
+        await client.setAlternativeUserID(clientAETitle);
+        await client.setUserName("");
+        await client.setUserIsRequestor(false);
+        await (await client.getRoleIDCode()).add(AuditMessages$RoleIDCode.Source);
+        await client.setNetworkAccessPointTypeCode(AuditMessages$NetworkAccessPointTypeCode.IPAddress);
+        await client.setNetworkAccessPointID(clientHostname);
+
+        /**
+        Participating Object:
+        Studies (1..N) 
+        存取的DICOM的Study。
+        */
+        let theStudies = [];
+        for (let i = 0; i < StudyInstanceUIDs.length; i++) {
+            let theStudy = new ParticipantObjectIdentification();
+            await theStudy.setParticipantObjectTypeCode(AuditMessages$ParticipantObjectTypeCode.SystemObject);
+            await theStudy.setParticipantObjectTypeCodeRole(AuditMessages$ParticipantObjectTypeCodeRole.Report);
+            await theStudy.setParticipantObjectIDTypeCode(AuditMessages$ParticipantObjectIDTypeCode.StudyInstanceUID);
+            await theStudy.setParticipantObjectID(StudyInstanceUIDs[i]);
+            let theSOPClass = await SOPClass.newInstanceAsync();
+            await theSOPClass.setUID(SOPClassUIDs[i]);
+            let theParticipantObjectDescription = await ParticipantObjectDescription.newInstanceAsync();
+            await (await theParticipantObjectDescription.getSOPClass()).add(theSOPClass);
+            await theStudy.setParticipantObjectDescription(theParticipantObjectDescription);
+            theStudies.push(theStudy);
+        }
+
+        /**
+        Participating Object: 
+        Patient (1)
+        存取的DICOM，其Patient。
+        */
+        let thePatient = new ParticipantObjectIdentification();
+        await thePatient.setParticipantObjectTypeCode(AuditMessages$ParticipantObjectTypeCode.Person);
+        await thePatient.setParticipantObjectTypeCodeRole(AuditMessages$ParticipantObjectTypeCodeRole.Patient);
+        await thePatient.setParticipantObjectIDTypeCode(AuditMessages$ParticipantObjectIDTypeCode.PatientNumber);
+        await thePatient.setParticipantObjectID(PatientID);
+        await thePatient.setParticipantObjectName(PatientName);
+
+        /**
+        將上述已經記錄完成的Real World Entities，組合成一個Audit Message。
+        */
+        let theActiveParticipants = [client, server];
+        let ParticipantObjects = [...theStudies, thePatient];
+        let msg = await AuditMessages.createMessage(theEvent, theActiveParticipants, ParticipantObjects);
+
+        return await AuditManager.toJson(msg);
+    }
+
     static async toJson(msg) {
         let msgJsonString = await Common.convertAuditMessageToJsonString(msg);
         let msgJson = JSON.parse(msgJsonString);
