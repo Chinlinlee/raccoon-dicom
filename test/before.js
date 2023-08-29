9dc4c562f772c1b4ea6fcc931e5540899bcb01a1
CHIN\a5566 <a5566qq2581@gmail.com>
2023-08-29 15:49:20 +0800

    test: fix missing audit message

--- test/before.js
+++ test/before.js
@@ -2,6 +2,8 @@ require("module-alias/register");
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const {getLogger} = require("log4js");
+require("../models/mongodb/models/auditMessage");
+require("../models/DICOM/dcm4che/java-instance");
 
 before(async() => {
     getLogger().level = "off";
