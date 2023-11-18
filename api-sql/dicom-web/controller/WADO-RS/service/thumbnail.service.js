abc07026583616bdd0a0bdf3698141ca1f95b53d
chinlinlee <a5566qq2581@gmail.com>
2023-11-18 21:53:53 +0800

    refactor: module alias for thumbnail APIs
    refactors the module alias for the thumbnail APIs in the
    WADO-RS service. It updates the require statements to use the new module
    alias in the thumbnail.service.js file. Additionally, it removes
    unnecessary code related to retrieving frame and instance thumbnails
    from the thumbnail folder.
    
    - With module alias, we don't need sql controller anymore

--- api-sql/dicom-web/controller/WADO-RS/service/thumbnail.service.js
+++ api-sql/dicom-web/controller/WADO-RS/service/thumbnail.service.js
@@ -1,156 +1,33 @@
-const errorResponse = require("@root/utils/errorResponse/errorResponseMessage");
 const renderedService = require("@rendered-service");
 const _ = require("lodash");
-const { 
-    ThumbnailService, 
-    StudyThumbnailFactory, 
-    SeriesThumbnailFactory, 
-    InstanceThumbnailFactory 
+const {
+    ThumbnailService,
+    StudyThumbnailFactory,
+    SeriesThumbnailFactory,
+    InstanceThumbnailFactory
 } = require("@root/api/dicom-web/controller/WADO-RS/service/thumbnail.service");
-const { InstanceModel } = require("@models/sql/models/instance.model");
-const { getUidsString } = require("@root/api/dicom-web/controller/WADO-RS/service/WADO-RS.service");
-class SqlThumbnailService extends ThumbnailService {
 
-    /**
-     * 
-     * @param {import("express").Request} req 
-     * @param {import("express").Response} res 
-     * @param {typeof ThumbnailFactory} thumbnailFactory 
-     */
-    constructor(req, res, apiLogger, thumbnailFactory) {
-        super(req, res, apiLogger, thumbnailFactory);
+ThumbnailService.prototype.getThumbnailByInstance = async function (instanceFramesObj) {
+    let dicomNumberOfFrames = _.get(instanceFramesObj, "x00280008", 1);
+    dicomNumberOfFrames = parseInt(dicomNumberOfFrames);
+    let medianFrame = 1;
+    if (dicomNumberOfFrames > 1) medianFrame = dicomNumberOfFrames >> 1;
+    if (this.request.params.frameNumber) {
+        medianFrame = this.request.params.frameNumber[0];
     }
 
-    async getThumbnailAndResponse() {
-        if (!_.get(this.request, "query.viewport")) {
-            _.set(this.request, "query.viewport", "100,100");
-        }
-
-        let instanceFramesObj = await this.thumbnailFactory.getThumbnailInstance();
-        if (this.checkInstanceExists(instanceFramesObj)) {
-            return;
-        }
-
-        let thumbnail = await this.getThumbnailByInstance(instanceFramesObj);
-        if (thumbnail) {
-            return this.response.end(thumbnail, "binary");
-        }
-        throw new Error(`Can not process this image, instanceUID: ${instanceFramesObj.instanceUID}`);
-    }
-
-    async getThumbnailByInstance(instanceFramesObj) {
-        let dicomNumberOfFrames = _.get(instanceFramesObj, "x00280008", 1);
-        dicomNumberOfFrames = parseInt(dicomNumberOfFrames);
-        let medianFrame = 1;
-        if (dicomNumberOfFrames > 1) medianFrame = dicomNumberOfFrames >> 1;
-        if (this.request.params.frameNumber) {
-            medianFrame = this.request.params.frameNumber[0];
-        }
-
-        let postProcessResult = await renderedService.postProcessFrameImage(this.request, medianFrame, instanceFramesObj);
-        if (postProcessResult.status) {
-            this.response.writeHead(200, {
-                "Content-Type": "image/jpeg"
-            });
-            this.apiLogger.logger.info(`Get instance's thumbnail successfully, instance UID: ${instanceFramesObj.instanceUID}`);
-            return postProcessResult.magick.toBuffer();
-        }
-        return undefined;
-    }
-
-    checkInstanceExists(instanceFramesObj) {
-        if (!instanceFramesObj) {
-            this.response.writeHead(404, {
-                "Content-Type": "application/dicom+json"
-            });
-            let notFoundMessage = errorResponse.getNotFoundErrorMessage(`Not Found, ${getUidsString(this.thumbnailFactory.uids)}`);
-
-            let notFoundMessageStr = JSON.stringify(notFoundMessage);
-
-            this.apiLogger.logger.warn(`[${notFoundMessageStr}]`);
-
-            return this.response.end(notFoundMessageStr);
-        }
-        return undefined;
-    }
-}
-
-
-class SqlStudyThumbnailFactory extends StudyThumbnailFactory {
-    constructor(uids) {
-        super(uids);
-    }
-
-    /**
-     * 
-     * @param {import("../../../../../utils/typeDef/dicom").Uids} uids 
-     */
-    async getThumbnailInstance() {
-        let medianInstance = await InstanceModel.getInstanceOfMedianIndex({
-            studyUID: this.uids.studyUID
+    let postProcessResult = await renderedService.postProcessFrameImage(this.request, medianFrame, instanceFramesObj);
+    if (postProcessResult.status) {
+        this.response.writeHead(200, {
+            "Content-Type": "image/jpeg"
         });
-        if (!medianInstance) return undefined;
-
-        let instanceFramesObj = await renderedService.getInstanceFrameObj({
-            studyUID: this.uids.studyUID,
-            seriesUID: medianInstance.seriesUID,
-            instanceUID: medianInstance.instanceUID
-        });
-
-        return instanceFramesObj;
+        this.apiLogger.logger.info(`Get instance's thumbnail successfully, instance UID: ${instanceFramesObj.instanceUID}`);
+        return postProcessResult.magick.toBuffer();
     }
+    return undefined;
+};
 
-}
-
-class SqlSeriesThumbnailFactory extends SeriesThumbnailFactory {
-    constructor(uids) {
-        super(uids);
-    }
-
-    /**
-     * 
-     * @param {import("../../../../../utils/typeDef/dicom").Uids} uids 
-     */
-    async getThumbnailInstance() {
-        let medianInstance = await InstanceModel.getInstanceOfMedianIndex({
-            studyUID: this.uids.studyUID,
-            seriesUID: this.uids.seriesUID
-        });
-        if (!medianInstance) return undefined;
-
-        let instanceFramesObj = await renderedService.getInstanceFrameObj({
-            studyUID: this.uids.studyUID,
-            seriesUID: this.uids.seriesUID,
-            instanceUID: medianInstance.instanceUID
-        });
-
-        return instanceFramesObj;
-    }
-
-}
-
-class SqlInstanceThumbnailFactory extends InstanceThumbnailFactory {
-    constructor(uids) {
-        super(uids);
-    }
-
-    /**
-     * 
-     * @param {import("../../../../../utils/typeDef/dicom").Uids} uids 
-     */
-    async getThumbnailInstance() {
-        let instanceFramesObj = await renderedService.getInstanceFrameObj({
-            studyUID: this.uids.studyUID,
-            seriesUID: this.uids.seriesUID,
-            instanceUID: this.uids.instanceUID
-        });
-
-        return instanceFramesObj;
-    }
-
-}
-
-module.exports.ThumbnailService = SqlThumbnailService;
-module.exports.StudyThumbnailFactory = SqlStudyThumbnailFactory;
-module.exports.SeriesThumbnailFactory = SqlSeriesThumbnailFactory;
-module.exports.InstanceThumbnailFactory = SqlInstanceThumbnailFactory;
+module.exports.ThumbnailService = ThumbnailService;
+module.exports.StudyThumbnailFactory = StudyThumbnailFactory;
+module.exports.SeriesThumbnailFactory = SeriesThumbnailFactory;
+module.exports.InstanceThumbnailFactory = InstanceThumbnailFactory;
