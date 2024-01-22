a731375d7e9e183184cb8b46de8f0f8d3d9f2bb8
chinlinlee <a5566qq2581@gmail.com>
2024-01-22 12:20:21 +0800

    feat: add `findOneByPatientID` to wrap mongoose `findOne` method

--- api/dicom-web/controller/PAM-RS/service/delete-patient.service.js
+++ api/dicom-web/controller/PAM-RS/service/delete-patient.service.js
@@ -11,7 +11,7 @@ class DeletePatientService {
 
     async delete() {
         let { patientID } = this.request.params;
-        let patient = await PatientModel.findOne({ patientID});
+        let patient = await PatientModel.findOneByPatientID(patientID);
         if (!patient) {
             throw new DicomWebServiceError(
                 DicomWebStatusCodes.NoSuchObjectInstance,
